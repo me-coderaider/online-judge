45678aa62b050ff7d58f9f661c75958f11559f3d
me-coderaider <kashyappankajnsp@gmail.com>
2023-09-18 00:12:29 +0530

    updated authentication navlink to LOGIN |16

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,31 +1,37 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../context/auth-context";
 
-import './NavLinks.css';
+import "./NavLinks.css";
 
-const NavLinks = (props) =>{
-    const auth=useContext(AuthContext);
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
 
-    return (
-        <ul className="nav-links">
-            <li>
-                <NavLink to="/" >HOME</NavLink>
-            </li>
-            <li>
-                <NavLink to="/problems">PROBLEMS</NavLink>
-            </li>
-            {auth.isLoggedIn && <li>
-                <NavLink to="/new_problem">ADD PROBLEM</NavLink>
-            </li>}
-            {!auth.isLoggedIn && <li>
-                <NavLink to="/auth">AUTHENTICATE</NavLink>
-            </li>}
-            {auth.isLoggedIn && <li>
-                <button onClick={auth.logout}>LOGOUT</button>
-            </li>}
-        </ul>
-    );
-}
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/">HOME</NavLink>
+      </li>
+      <li>
+        <NavLink to="/problems">PROBLEMS</NavLink>
+      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/new_problem">ADD PROBLEM</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">LOGIN</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
+    </ul>
+  );
+};
 
 export default NavLinks;
